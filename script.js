55e2e98532a574e3e6168cf2878797bcbf764fcc
Denis <kubikirubiki228@gmail.com>
2025-09-08 11:09:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const section = document.getElementById('flex_section');
-const project = 3;
+const project = 4;
 for(i=1; i<1+project;i++) {
     const tekstDiv = `<a href="https://romaniukd.github.io/Project-0${i}/"><div class="flex_div button"><h2>Project 0${i}</h2></div>`
     section.innerHTML += tekstDiv
 }
+
